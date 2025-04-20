53bc70e8ef7d97aa3d1d5586fe69ac45681cd38b
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-20 19:38:10 +0600

    just check to it's working or not]

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+      <Person></Person>
+    
+     
     </>
   )
 }
 
+function Person(){
+  return (
+   <div>
+     <p>polash</p>
+     <h1>ahasan habib polash he is a very nice hearted person</h1>
+     <h4>hi there this is me</h4>
+     <h6>it's my first react native project that i prepare to done</h6>
+   </div>
+  )
+}
 export default App
