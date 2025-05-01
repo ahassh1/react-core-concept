d06d8a294624e0cff0d99b73b9d0880b2281cf09
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-02 01:15:04 +0600

    explore jsx and props basic

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ function App() {
     <>
     
       <Person></Person>
+      <Student name='ahasan' tech='js'></Student>
+      <Student name= 'habib' tech='java'></Student>
+      <Student name= 'ajof' tech='python'></Student>
+      <Developer></Developer>
+    
     
      
     </>
@@ -15,12 +20,47 @@ function App() {
 
 function Person(){
   return (
-   <div>
+   <div className='first'>
      <p>polash</p>
+     <div style={{
+      color:"red", 
+      fontSize: '20px'
+     }
+     }>
      <h1>ahasan habib polash he is a very nice hearted person</h1>
      <h4>hi there this is me</h4>
-     <h6>it's my first react native project that i prepare to done</h6>
+     </div>
+     <h5>it's my first react native project that i prepare to done</h5>
    </div>
   )
 }
+
+ function Student(props){
+  console.log(props)
+
+  return(
+    <div className='students'>
+      <h1>hi there </h1>
+      <h2> hello</h2>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam architecto, asperiores voluptates maxime numquam cum beatae ipsa similique veritatis, at minima.</h1>
+      <h3>Developer: {props.name}</h3>
+      <p>Technology:{props.tech}</p>
+    </div>
+  )
+ }
+
+  function Developer(){
+    const name= 'polash';
+    const age= 20;
+
+    const newStyle = {
+      color:'tomato',
+      fontSize: '23px',
+      textAlign:'left',
+      margin: '20px'
+    }
+    return(
+         <p style={newStyle}>I am the person : {name} {age}</p>
+    )
+  }
 export default App
